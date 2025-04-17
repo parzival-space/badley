f88abe4a3422bbd332d7cbc771da0f6432302266
Malte Linke (parzival-space) <me@parzival.space>
2025-04-17 20:26:02 +0200

    refactor configuration files and add Docker support

--- src/test/java/space/parzival/discord/badley/BadleyApplicationTests.java
+++ src/test/java/space/parzival/discord/badley/BadleyApplicationTests.java
@@ -1,13 +1,5 @@
 package space.parzival.discord.badley;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class BadleyApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
