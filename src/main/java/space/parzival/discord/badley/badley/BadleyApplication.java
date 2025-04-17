bf8e93076793a0bdd550cccfd732d62cba114fb4
Malte Linke (parzival-space) <me@parzival.space>
2025-04-17 18:59:04 +0200

    initialize project with Spring Boot and Maven Wrapper

---
+++ src/main/java/space/parzival/discord/badley/badley/BadleyApplication.java
@@ -0,0 +1,13 @@
+package space.parzival.discord.badley.badley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadleyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadleyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/space/parzival/discord/badley/badley/BadleyApplicationTests.java
@@ -0,0 +1,13 @@
+package space.parzival.discord.badley.badley;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadleyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
