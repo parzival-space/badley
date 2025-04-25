7e77a1706ef7e790b9e3eda0dc5e8039855bdb68
Malte Linke (parzival-space) <me@parzival.space>
2025-04-25 22:28:09 +0200

    Add Google service integration with properties and response models

---
+++ src/main/java/space/parzival/discord/badley/service/google/model/query/GoogleQueryResult.java
@@ -0,0 +1,46 @@
+package space.parzival.discord.badley.service.google.model.query;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.*;
+
+@Value
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@With
+@JsonDeserialize(builder = GoogleQueryResult.GoogleQueryResultBuilder.class)
+public class GoogleQueryResult {
+    @JsonProperty("kind")
+    String kind;
+
+    @JsonProperty("title")
+    String title;
+
+    @JsonProperty("htmlTitle")
+    String htmlTitle;
+
+    @JsonProperty("link")
+    String link;
+
+    @JsonProperty("displayLink")
+    String displayLink;
+
+    @JsonProperty("snippet")
+    String snippet;
+
+    @JsonProperty("htmlSnippet")
+    String htmlSnippet;
+
+    @JsonProperty("formattedUrl")
+    String formattedUrl;
+
+    @JsonProperty("htmlFormattedUrl")
+    String htmlFormattedUrl;
+
+    /**
+     * @apiNote DO NOT USE THIS FIELD!
+     * @implNote This field will not be parsed as it contains redundant information.
+     */
+    @JsonProperty("pagemap")
+    Object pageMap;
+}
