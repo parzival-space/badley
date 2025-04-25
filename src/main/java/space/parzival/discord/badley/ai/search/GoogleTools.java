f0bd60c27f932fb04dd6c72c0b3dfcf7fa7a1aac
Malte Linke (parzival-space) <me@parzival.space>
2025-04-25 22:39:40 +0200

    Add GoogleTools for web search integration and corresponding tests

---
+++ src/main/java/space/parzival/discord/badley/ai/search/GoogleTools.java
@@ -0,0 +1,42 @@
+package space.parzival.discord.badley.ai.search;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+import space.parzival.discord.badley.ai.AiToolsService;
+import space.parzival.discord.badley.service.google.GoogleService;
+
+import java.util.stream.Collectors;
+
+@Slf4j
+@Component
+@ConditionalOnProperty(value = "badley.ai.tools.google.token")
+@AllArgsConstructor
+public class GoogleTools implements AiToolsService {
+    private final GoogleService googleService;
+
+    private static final String WEB_SEARCH_RESULT = """
+            %s:
+            - Title: %s
+            - Description: %s
+            """;
+
+    @Tool(description = "Search the web for information.")
+    public String searchGoogle(String query) {
+        log.debug("AI is requesting web search results from Google for query: {}", query);
+
+        try {
+            var response = googleService.query(query);
+
+            return response.getItems().stream().map(result ->
+                    String.format(WEB_SEARCH_RESULT,
+                            result.getLink(), result.getTitle(), result.getSnippet()))
+                    .collect(Collectors.joining("\n"));
+        } catch (Exception e) {
+            log.error("Error fetching web search results: {}", e.getMessage());
+            return "Error fetching web search results: " + e.getMessage();
+        }
+    }
+}
