f7727179d291ef5965fa74d706858b7c141b5ae8
Malte Linke (parzival-space) <me@parzival.space>
2025-04-30 00:16:15 +0200

    Use application name from configuration in Google Custom Search API setup

--- src/main/java/space/parzival/discord/badley/configuration/ai/google/GoogleSearchConfiguration.java
+++ src/main/java/space/parzival/discord/badley/configuration/ai/google/GoogleSearchConfiguration.java
@@ -5,6 +5,7 @@ import com.google.api.client.http.GenericUrl;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.customsearch.v1.CustomSearchAPI;
 import com.google.api.services.customsearch.v1.CustomSearchAPIRequestInitializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -17,13 +18,16 @@ import java.util.Optional;
 @Configuration
 public class GoogleSearchConfiguration {
 
+    @Value("${spring.application.name}")
+    private String appName;
+
     @Bean
     public CustomSearchAPI googleCustomSearchAPI(GoogleProperties properties) throws GeneralSecurityException, IOException {
         return new CustomSearchAPI.Builder(
             GoogleNetHttpTransport.newTrustedTransport(),
             GsonFactory.getDefaultInstance(),
             null)
-                .setApplicationName("Badley")
+                .setApplicationName(appName)
                 .setCustomSearchAPIRequestInitializer(
                     new CustomSearchAPIRequestInitializer(properties.getToken()))
                 .setHttpRequestInitializer(httpRequest -> {
