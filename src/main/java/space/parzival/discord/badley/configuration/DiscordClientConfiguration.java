f7db6057925ab0bbf31fe0f96ccf95661af81753
Malte Linke (parzival-space) <me@parzival.space>
2025-05-23 20:23:02 +0200

    Improve event logging for Discord client

--- src/main/java/space/parzival/discord/badley/configuration/DiscordClientConfiguration.java
+++ src/main/java/space/parzival/discord/badley/configuration/DiscordClientConfiguration.java
@@ -4,14 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.ExceptionEvent;
+import net.dv8tion.jda.api.events.GatewayPingEvent;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
+import net.dv8tion.jda.api.events.session.SessionDisconnectEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.CloseCode;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import space.parzival.discord.badley.configuration.properties.DiscordClientProperties;
 
+import javax.annotation.Nonnull;
 import java.util.EnumSet;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Configuration
@@ -29,8 +37,31 @@ public class DiscordClientConfiguration {
                     GatewayIntent.GUILD_MEMBERS,
                     GatewayIntent.MESSAGE_CONTENT
                 ))
-            .addEventListeners(discordEventListeners != null ? discordEventListeners.toArray() : List.of().toArray())
             .setAutoReconnect(discordClientProperties.isAutoReconnect())
+            .addEventListeners(discordEventListeners != null ? discordEventListeners.toArray() : List.of().toArray())
+            .addEventListeners(new ListenerAdapter() {
+                @Override
+                public void onException(@Nonnull ExceptionEvent event) {
+                    log.error("Exception in JDA event: ", event.getCause());
+                }
+
+                @Override
+                public void onReady(@Nonnull ReadyEvent event) {
+                    log.info("Connected to Discord as {}#{}", event.getJDA().getSelfUser().getName(),
+                        event.getJDA().getSelfUser().getDiscriminator());
+                }
+
+                @Override
+                public void onSessionDisconnect(@Nonnull SessionDisconnectEvent event) {
+                    log.warn("Disconnected from Discord with Code {}: {}",
+                        Optional.ofNullable(event.getCloseCode())
+                            .map(c -> String.valueOf(c.getCode()))
+                            .orElse("Unknown"),
+                        Optional.ofNullable(event.getCloseCode())
+                            .map(CloseCode::getMeaning)
+                            .orElse("Unknown"));
+                }
+            })
             .build();
     }
 }
