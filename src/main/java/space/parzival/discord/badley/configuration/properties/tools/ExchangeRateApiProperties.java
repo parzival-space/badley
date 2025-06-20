d985366bdf63e17b9a4bdd24e684f618780eaeb1
Malte Linke (parzival-space) <me@parzival.space>
2025-06-21 01:08:57 +0200

    Remove unused AllArgsConstructor annotation from ExchangeRateApiProperties

--- src/main/java/space/parzival/discord/badley/configuration/properties/tools/ExchangeRateApiProperties.java
+++ src/main/java/space/parzival/discord/badley/configuration/properties/tools/ExchangeRateApiProperties.java
@@ -1,7 +1,6 @@
 package space.parzival.discord.badley.configuration.properties.tools;
 
 import jakarta.annotation.Nullable;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
@@ -9,7 +8,6 @@ import javax.validation.constraints.NotNull;
 
 @Data
 @ConfigurationProperties(prefix = "badley.ai.tools.exchange-rate-api")
-@AllArgsConstructor
 public class ExchangeRateApiProperties {
     /**
      * Whether the Exchange Rate API integration is enabled.
